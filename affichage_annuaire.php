11d82b2698424f6d82a83ccc5b5295dd6330cbcb
ciceron1971 <jean-francois.lepante@live.fr>
2021-05-23 23:15:30 +0200

    intégration de la suppression des données dans la bdd

--- affichage_annuaire.php
+++ affichage_annuaire.php
@@ -17,14 +17,18 @@ require_once('fonction.php');
 <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script src="./datepicker-fr.js"></script>
+<link rel="stylesheet" href="./style.css">
 
-<link rel="stylesheet" href="assets/css/style.css">
 <title>Affichage Annuaire</title>
 </head>
 <body>
 <div class="container text-center"><h1>Affichage Annuaire</h1></div>
+<hr class="mb-3">
 <?php
 
+
+    if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['tele'])){
+        $errors = 0;
 
         $nom            = valid_donnees($_POST['nom']);
         $prenom         = valid_donnees($_POST['prenom']);
@@ -36,15 +40,13 @@ require_once('fonction.php');
         $date_naissance = valid_donnees($_POST['datepicker']);
         $description    = valid_donnees($_POST['description']);
 
-function valid_donnees ($donnees){
-    $donnees = trim($donnees);
-    $donnees = stripslashes($donnees);
-    $donnees = htmlspecialchars($donnees);
-    return $donnees;
-}
+        function valid_donnees ($donnees){
+        $donnees = trim($donnees);
+        $donnees = stripslashes($donnees);
+        $donnees = htmlspecialchars($donnees);
+        return $donnees;
+        }
 
-    if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['tele'])){
-        $errors = 0;
         // $reponse = $pdo->query
         sql("INSERT INTO annuaire ( nom, prenom, tele, profession, ville, cp, adresse, date_naissance, description ) 
             VALUES ('$nom', '$prenom', '$tele', '$profession', '$ville', '$cp', '$adresse', '$date_naissance','$description')");
@@ -57,10 +59,68 @@ function valid_donnees ($donnees){
     { 
         add_flash('Merci de rentrer un nom','danger'); 
     }
+    echo'<form action="" method="get">';
+    echo'<div class="container mx-0 px-0">';
+        echo'<div class="row">';
+    //Affichage des colonnes de la table
+
+    $reponse = $pdo->query("SELECT * FROM annuaire");
+    echo'<div class="col-11">';
+    echo '<table class="table table-striped table-hover table-sm" style="border-collapse : collapse; width: 100%; ">';
+
+    echo '<tr>';
+        for($i = 0; $i < $reponse->columnCount(); $i++) {
+            $infos_colonne = $reponse->getColumnMeta($i);
+            echo '<th>' . ucfirst($infos_colonne['name']) . '</th>';
+        }
+// ucfirst() pour avoir la première lettre en majuscule
+    echo '</tr>';
+
+    while($ligne = $reponse->fetch(PDO::FETCH_ASSOC)) {
+        echo '<tr>';
+
+        foreach($ligne AS $valeur) {
+                echo '<td contenteditable="true">' . $valeur . '</td>';        
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+    echo'</div>';
+    
+   
+    $reponse2 = $pdo->query("SELECT * FROM annuaire");
+    echo'<div class="col">';
+        echo'<table class="table table-striped table-sm" style="border-collapse : collapse; width: 100%; ">';
+        echo '<tr ><th class="text-center"><i class="fas fa-edit" data-bs-toggle="tooltip" data-bs-placement="top" title="Mettre à jour"></i></th></tr>';
+    while($ligne2 = $reponse2->fetch(PDO::FETCH_ASSOC)) {
+            echo '<tr class="text-center">';
+        {
+                echo '<td class"py-0" ><a href="?action=delete&id='.$ligne2['id'].'" class="btn btn-danger confirm fa fa-times py-0" style=" height: 1.1rem; width:3rem"></a></td>';        
+        }
+            echo '</tr>';
+    }
+            echo'</table>';
+            echo'</div>';
+        echo'</div>';
+echo'</div>';
+echo'</form>';
+//Traitement des suppression de données du tableau du repertoire
+    if(isset($_GET['action']) && $_GET['action'] == 'delete' && !empty($_GET['id']) && is_numeric($_GET['id']))
+                {
+                  sql("DELETE FROM annuaire WHERE id=:id", array(
+                        'id' => $_GET['id']
+                  )) ;
+                  add_flash('La catégorie a été supprimé','success');
+                  header('location:'.$_SERVER['PHP_SELF']).
+                    exit();
+
+
+                }
+
+
 
 
 ?>
-
 
 
 
--- annuaire.php
+++ annuaire.php
@@ -9,6 +9,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+<link rel="stylesheet" href="style.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script src="./datepicker-fr.js"></script>
 <!-- option locale du datepicker -->
@@ -21,7 +22,7 @@
     });
   } );
 </script>
-<link rel="stylesheet" href="assets/css/style.css">
+
 <title>Repertoire</title>
 </head>
 <body>
@@ -30,52 +31,53 @@
 
 
 <div class="container text-center"><h1>REPERTOIRE</h1></div>
+<hr class="mb-3">
 
 <div class="col-4 mx-auto">
     <form action="" method="post" enctype="multipart/form-data">
         <div class="mb-1">
             <label for="nom" class="col-sm-2 col-form-label">Nom</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="nom"  name="nom" placeholder="indiquez votre nom">
+                    <input type="text" class="form-control border1" id="nom"  name="nom" placeholder="indiquez le nom">
                 </div>
         </div>
 
         <div class="mb-1">
             <label for="prenom" class="col-sm-2 col-form-label">Prenom</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="prenom" name="prenom" placeholder="indiquez votre prenom">
+                    <input type="text" class="form-control border1" id="prenom" name="prenom" placeholder="indiquez le prenom">
                 </div>
         </div>
         <div class="mb-1">
             <label for="profession" class="col-sm-2 col-form-label">Profession</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="profession" name="profession" placeholder="indiquez la profession">
+                    <input type="text" class="form-control border1" id="profession" name="profession" placeholder="indiquez la profession">
                 </div>
         </div>
      <!-- Auto-complétion des villes -->
         <div class="mb-1">
             <label for="ville" class="col-sm-2 col-form-label">Ville</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="ville"  name="ville" placeholder="indiquez la ville">
+                    <input type="text" class="form-control border1" id="ville"  name="ville" placeholder="indiquez la ville">
                 </div>
         </div>
         <!-- Auto-complétion des codes postaux -->
         <div class="mb-1">
             <label for="cp" class="col-sm-2 col-4 form-label">Code postal</label> 
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="cp" value="" name="cp" placeholder="indiquez le code postal">
+                    <input type="text" class="form-control border1" id="cp" value="" name="cp" placeholder="indiquez le code postal">
                 </div>
         </div>
         <div class="mb-1">
             <label for="adresse" class="col-sm-10 col-form-label">Adresse</label>
                 <div class="col-sm-10">
-                    <textarea type="text" class="form-control" id="adresse" value="" name="adresse"  rows="3"></textarea>
+                    <textarea type="text" class="form-control border1" id="adresse" value="" name="adresse"  rows="3"></textarea>
                 </div>
         </div>
         <div class="mb-1">
             <label for="datepicker" class="col-sm-2 col-4 form-label">La date de naissance</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control w-50" id="datepicker" name="datepicker">
+                    <input type="text" class="form-control w-50 border1" id="datepicker" name="datepicker">
                     <select id="locale" style="visibility:hidden">
                         <option value="ar">Arabic (&#x202B;(&#x627;&#x644;&#x639;&#x631;&#x628;&#x64A;&#x629;</option>
                         <option value="zh-TW">Chinese Traditional (&#x7E41;&#x9AD4;&#x4E2D;&#x6587;)</option>
@@ -98,18 +100,15 @@
             </label>
         </div>
 
-
-
-
         <div class="mb-1">
             <label for="description" class="col-sm-2 col-4 form-label">Description</label>
                 <div class="col-sm-10">
-                    <textarea type="text" class="form-control" id="description" value="" name="description" placeholder=" Description" rows="2"></textarea>
+                    <textarea type="text" class="form-control border1" id="description" value="" name="description" placeholder=" Description" rows="2"></textarea>
                 </div>
         </div>
 
         <div class="col-12">
-             <button class="btn btn-secondary" type="submit">validez</button>
+             <button class="btn btn-secondary border1" type="submit">validez</button>
         </div>
  
     </form>
